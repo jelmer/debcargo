5d270cf43e9245e9e8939a10bf6cad468f78e38b
Vasudev Kamath <vasudev@copyninja.info>
2017-06-08 17:18:54 +0530

    Move more debian related parts to debian module.

--- src/bin/debcargo.rs
+++ src/bin/debcargo.rs
@@ -10,188 +10,24 @@ extern crate semver_parser;
 extern crate tar;
 extern crate tempdir;
 
-use cargo::core::{Dependency, Registry, Source, TargetKind};
+use cargo::core::{Dependency, Source};
 use clap::{App, AppSettings, ArgMatches, SubCommand};
 use itertools::Itertools;
 use semver::Version;
 use std::collections::{HashMap, HashSet};
 use std::fmt::{self, Write as FmtWrite};
 use std::fs;
-use std::hash::{Hash, Hasher};
 use std::io::{self, Write as IoWrite};
 use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
+use std::iter::FromIterator;
 
 use debcargo::errors::*;
 use debcargo::copyright;
 use debcargo::crates::CrateInfo;
-
-const RUST_MAINT: &'static str = "Rust Maintainers <pkg-rust-maintainers@lists.alioth.debian.org>";
-
-fn hash<H: Hash>(hashable: &H) -> u64 {
-    #![allow(deprecated)]
-    let mut hasher = std::hash::SipHasher::new();
-    hashable.hash(&mut hasher);
-    hasher.finish()
-}
-
-/// Translates a semver into a Debian version. Omits the build metadata, and uses a ~ before the
-/// prerelease version so it compares earlier than the subsequent release.
-fn deb_version(v: &Version) -> String {
-    let mut s = format!("{}.{}.{}", v.major, v.minor, v.patch);
-    for (n, id) in v.pre.iter().enumerate() {
-        write!(s, "{}{}", if n == 0 { '~' } else { '.' }, id).unwrap();
-    }
-    s
-}
-
-#[derive(PartialEq)]
-enum V { M(u64), MM(u64, u64), MMP(u64, u64, u64) }
-
-impl V {
-    fn inclast(&self) -> V {
-        use V::*;
-        match *self {
-            M(major) => M(major+1),
-            MM(major, minor) => MM(major, minor+1),
-            MMP(major, minor, patch) => MMP(major, minor, patch+1),
-        }
-    }
-}
-
-impl fmt::Display for V {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use V::*;
-        match *self {
-            M(major) => write!(f, "{}", major),
-            MM(major, minor) => write!(f, "{}.{}", major, minor),
-            MMP(major, minor, patch) => write!(f, "{}.{}.{}", major, minor, patch),
-        }
-    }
-}
-
-fn deb_name(name: &str) -> String {
-    format!("librust-{}-dev", name.replace('_', "-"))
-}
-
-fn deb_feature_name(name: &str, feature: &str) -> String {
-    format!("librust-{}+{}-dev", name.replace('_', "-"), feature.replace('_', "-"))
-}
-
-/// Translates a Cargo dependency into a Debian package dependency.
-fn deb_dep(dep: &Dependency) -> Result<String> {
-    use semver_parser::range::*;
-    use semver_parser::range::Op::*;
-    use self::V::*;
-    let dep_dashed = dep.name().replace('_', "-");
-    let mut suffixes = Vec::new();
-    if dep.uses_default_features() {
-        suffixes.push("+default-dev".to_string());
-    }
-    for feature in dep.features() {
-        suffixes.push(format!("+{}-dev", feature.replace('_', "-")));
-    }
-    if suffixes.is_empty() {
-        suffixes.push("-dev".to_string());
-    }
-    let req = semver_parser::range::parse(&dep.version_req().to_string()).unwrap();
-    let mut deps = Vec::new();
-    for suffix in suffixes {
-        let pkg = |v: &V| {
-            let (major, minor) = match *v {
-                M(major) => (major, 0),
-                MM(major, minor) | MMP (major, minor, _) => (major, minor),
-            };
-            if major == 0 {
-                format!("librust-{}-{}.{}{}", dep_dashed, major, minor, suffix)
-            } else {
-                format!("librust-{}-{}{}", dep_dashed, major, suffix)
-            }
-        };
-        for p in &req.predicates {
-            // Cargo/semver and Debian handle pre-release versions quite differently, so a versioned
-            // Debian dependency cannot properly handle pre-release crates.  Don't package pre-release
-            // crates or crates that depend on pre-release crates.
-            if !p.pre.is_empty() {
-                bail!("Dependency on prerelease version: {} {:?}", dep.name(), p);
-            }
-            let mmp = match (p.minor, p.patch) {
-                (None, None) => M(p.major),
-                (Some(minor), None) => MM(p.major, minor),
-                (Some(minor), Some(patch)) => MMP(p.major, minor, patch),
-                (None, Some(_)) => panic!("semver had patch without minor"),
-            };
-            if mmp == M(0) && p.op != Gt {
-                bail!("Unrepresentable dependency version predicate: {} {:?}", dep.name(), p);
-            }
-            match (&p.op, &mmp) {
-                (&Ex, &M(..)) => deps.push(pkg(&mmp)),
-                (&Ex, &MM(..)) => deps.push(format!("{} (>= {})", pkg(&mmp), mmp)),
-                (&Ex, &MMP(..)) => {
-                    deps.push(format!("{} (>= {})", pkg(&mmp), mmp));
-                    deps.push(format!("{} (<< {})", pkg(&mmp), mmp.inclast()));
-                }
-                // We can't represent every major version that satisfies an inequality, because
-                // each major version has a different package name, so we only allow the first
-                // major version that satisfies the inequality. This may result in a stricter
-                // dependency, but will never result in a looser one.  We could represent some
-                // dependency ranges (such as >= x and < y) better with a disjunction on multiple
-                // package names, but that would break when depending on multiple features.
-                (&Gt, &M(_)) | (&Gt, &MM(0, _)) => deps.push(pkg(&mmp.inclast())),
-                (&Gt, _) => deps.push(format!("{} (>> {})", pkg(&mmp), mmp)),
-                (&GtEq, &M(_)) | (&GtEq, &MM(0, _)) => deps.push(pkg(&mmp)),
-                (&GtEq, _) => deps.push(format!("{} (>= {})", pkg(&mmp), mmp)),
-                (&Lt, &M(major)) => deps.push(pkg(&M(major-1))),
-                (&Lt, &MM(0, 0)) => bail!("Unrepresentable dependency version predicate: {} {:?}", dep.name(), p),
-                (&Lt, &MM(0, minor)) => deps.push(pkg(&MM(0, minor-1))),
-                (&Lt, _) => deps.push(format!("{} (<< {})", pkg(&mmp), mmp)),
-                (&LtEq, &M(_)) | (&LtEq, &MM(0, _)) => deps.push(pkg(&mmp)),
-                (&LtEq, _) => deps.push(format!("{} (<< {})", pkg(&mmp), mmp.inclast())),
-                (&Tilde, &M(_)) | (&Tilde, &MM(0, _)) | (&Tilde, &MMP(0, _, 0)) => deps.push(pkg(&mmp)),
-                (&Tilde, &MM(..)) | (&Tilde, &MMP(0, _, _)) => deps.push(format!("{} (>= {})", pkg(&mmp), mmp)),
-                (&Tilde, &MMP(major, minor, _)) => {
-                    deps.push(format!("{} (>= {})", pkg(&mmp), mmp));
-                    deps.push(format!("{} (<< {})", pkg(&mmp), MM(major, minor+1)));
-                }
-                (&Compatible, &MMP(0, 0, _)) => {
-                    deps.push(format!("{} (>= {})", pkg(&mmp), mmp));
-                    deps.push(format!("{} (<< {})", pkg(&mmp), mmp.inclast()));
-                }
-                (&Compatible, &M(_)) | (&Compatible, &MM(0, _)) | (&Compatible, &MM(_, 0)) | (&Compatible, &MMP(0, _, 0)) => deps.push(pkg(&mmp)),
-                (&Compatible, &MM(..)) | (&Compatible, &MMP(..)) => deps.push(format!("{} (>= {})", pkg(&mmp), mmp)),
-                (&Wildcard(WildcardVersion::Major), _) => bail!("Unrepresentable dependency wildcard: {} = \"{:?}\"", dep.name(), p),
-                (&Wildcard(WildcardVersion::Minor), _) => deps.push(pkg(&mmp)),
-                (&Wildcard(WildcardVersion::Patch), _) => deps.push(format!("{} (>= {})", pkg(&mmp), mmp)),
-            }
-        }
-    }
-    Ok(deps.join(", "))
-}
-
-/// Retrieve one of a series of environment variables, and provide a friendly error message for
-/// non-UTF-8 values.
-fn get_envs(keys: &[&str]) -> Result<Option<String>> {
-    use std::env::VarError;
-    for key in keys {
-        match std::env::var(key) {
-            Ok(val) => { return Ok(Some(val)); }
-            Err(e@VarError::NotUnicode(_)) => {
-                return Err(e).chain_err(|| format!("Environment variable ${} not valid UTF-8", key));
-            }
-            Err(VarError::NotPresent) => {},
-        }
-    }
-    Ok(None)
-}
-
-/// Determine a name and email address from environment variables.
-fn get_deb_author() -> Result<String> {
-    let name = try!(try!(get_envs(&["DEBFULLNAME", "NAME"]))
-                    .ok_or("Unable to determine your name; please set $DEBFULLNAME or $NAME"));
-    let email = try!(try!(get_envs(&["DEBEMAIL", "EMAIL"]))
-                     .ok_or("Unable to determine your email; please set $DEBEMAIL or $EMAIL"));
-    Ok(format!("{} <{}>", name, email))
-}
+use debcargo::debian::{PkgBase, Source as ControlSource, Package as
+                       ControlPackage};
+use debcargo::debian::{get_deb_author, deb_feature_name};
 
 /// Write a Description field with proper formatting.
 fn write_description<W: IoWrite>(out: &mut W, summary: &str, longdesc: Option<&String>, boilerplate: Option<&String>) -> Result<()> {
