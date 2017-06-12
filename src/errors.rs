96e443c61c019f1e06a9bcc43198f000c7a7f735
Vasudev Kamath <vasudev@copyninja.info>
2017-06-12 16:36:51 +0530

    Some handy macros for colored output
    For now debcargo_bail! is not used might drop it later.

--- src/errors.rs
+++ src/errors.rs
@@ -13,3 +13,44 @@ error_chain! {
         String(string::FromUtf8Error);
     }
 }
+
+#[macro_export]
+macro_rules! debcargo_info {
+    ($e:expr) => {{
+        let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+        writeln!(&mut stdout, $e)?;
+    }};
+
+    ($fmt:expr, $($arg:tt)+) => {{
+        let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+        writeln!(&mut stdout, "{}", format!($fmt, $($arg)+))?;
+    }};
+}
+
+#[macro_export]
+macro_rules! debcargo_highlight {
+    ($e:expr) => {{
+        let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red).set_bold(true).set_intense(true)))?;
+        writeln!(&mut stdout, $e)?;
+    }};
+
+    ($fmt:expr, $($arg:tt)+) => {{
+        let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)).set_bold(true).set_intense(true))?;
+        writeln!(&mut stdout, "{}", format!($fmt, $($arg)+))?;
+    }};
+}
+
+#[macro_export]
+macro_rules! debcargo_bail {
+    ($e:expr) => {
+        return Err(debcargo_bail!($e));
+    };
+
+    ($fmt:expr, $($arg:tt)+) => {
+        return Err(debcargo_bail!($fmt, $($arg)+));
+    }
+}
