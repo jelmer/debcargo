0e4aea5ee67781da8b44b30b399498c9f1f16d62
Andreas Rottmann <a.rottmann@gmx.at>
2019-09-09 01:56:27 +0200

    Fix up handling of "or-later" license variants
    Before this change, `debcargo` would:
    
    - Attempt to split versions on the "or" occurring inside the
      "-or-later" suffix.
    
      This is rectified by adjusting the regex to insist on a space left
      and right of the "and" and "or" operators.
    
    - Not realize that "-or-later" licenses are actually variants of
      licenses it knows about, and hence incorrectly categorizes them as
      "unknown".
    
      This is fixed by just stripping the "-or-later" suffix, if
      present. The code does hence not detect invalid license strings such
      as "zlib-or-later". However, it's arguably not debcargo's job to
      check for bogus license identifiers, as that should be done by the
      packager anyway, and is also done by crates.io upon upload,
      according to the cargo book.
    
    Both changes are, very lightly, but still, covered by a newly-added
    unit test.

---
+++ src/debian/copyright/tests.rs
@@ -0,0 +1,21 @@
+use super::get_licenses;
+
+#[test]
+fn check_get_licenses() {
+    let test_data: &[(&str, &[(&str, bool)])] = &[
+        ("AGPL-3.0", &[("AGPL-3.0", true)]),
+        ("AcmeCorp-1.0", &[("AcmeCorp-1.0", false)]),
+        ("AGPL-3.0-or-later", &[("AGPL-3.0-or-later", true)]),
+        ("Apache-2.0/MIT", &[("Apache-2.0", true), ("MIT", true)]),
+        ("Apache-2.0 or MIT", &[("Apache-2.0", true), ("MIT", true)]),
+        ("FooBar-1.0 AND MIT", &[("FooBar-1.0", false), ("MIT", true)]),
+    ];
+    for (name, expected) in test_data {
+        let licenses = get_licenses(name).expect("getting licenses failed");
+        let found: Vec<_> = licenses
+            .iter()
+            .map(|l| (l.name.as_str(), !l.text.starts_with("FIXME")))
+            .collect();
+        assert_eq!(&found[..], &expected[..]);
+    }
+}
